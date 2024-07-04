42c7602df3ceabac4c2e52b52e48402c1530297d
ujsquared <ujsquared@gmail.com>
2024-07-04 15:10:39 +0530

    add dynamic string functionality; remove other flicker issues

--- kilo.c
+++ kilo.c
@@ -5,7 +5,7 @@
 #include<unistd.h>
 #include<errno.h>
 #include<sys/ioctl.h>
-#include<ctype.h>
+#include<string.h>
 #include<termios.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -53,47 +53,78 @@ char editorReadKey(){
     return c;
 }
 int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i = 0 ;
+
     if(write(STDOUT_FILENO, "\x1b[6n", 4) !=4) return -1;
 
-    printf("\r\n");
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1){
-        if (iscntrl(c)){
-            printf("%d \r\n",c);
-        }
-        else{
-            printf("%d ('%c')\r\n",c,c);
-        }
+    while(i< sizeof(buf) -1){
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if(buf[i] == 'R') break;
+        i++;
     }
-    editorReadKey();
-    return -1;
-    }
+    buf[i] = '\0';
+    if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if(sscanf(&buf[2], "%d;%d", rows, cols) !=2) return -1;
+    return 0;
+}
 int getWindowsSize(int *rows, int *cols){
     struct winsize ws;
     if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
         if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12){
-        return getCursorPosition(rows,cols);
+            return -1;
         } 
+        return getCursorPosition(rows, cols); 
     } else{
         *cols = ws.ws_col;
         *rows = ws.ws_row;
         return 0;
     }
 }
+/*append buffer */
+// this section creates functions to implement the functionality of dynamic strings to our program
+// this is important because instead of using multiple write function which may cause flicker issue, we will use single write 
+// on a string that we'll keep adding and mutating. we'll call this string a buffer. 
+
+struct abuf{
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len){
+    char *new = realloc(ab->b , ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s,len);
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
 /* output */
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
     int y;
     for(y = 0; y<E.screenrows; y++){
-        write(STDOUT_FILENO, "=\r\n",3);
+        abAppend(ab, "=", 1);
+        if(y < E.screenrows - 1){
+            abAppend(ab, "\r\n", 2);
+        }
     }
 }
 void editorRefreshScreen(){ 
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct abuf ab = ABUF_INIT;
+    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[H", 3);
     
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
-   // write(STDOUT_FILENO, " Ujjwal Kala Samjha Kya bidhu", 30);/* it comes out to be 30 as there are 29 characters and last character is ending character so ek byte uski */
+    editorDrawRows(&ab);
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, " Ujjwal Kala Samjha Kya bidhu", 30);/* it comes out to be 30 as there are 29 characters and last character is ending character so ek byte uski */
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 /*input */
 void editorProcessKeypress(){
